3b26d436a13d1bcc13ee18e1861bd85cec5f0dbc
armgnarda <93938720+armgnarda@users.noreply.github.com>
2024-05-06 21:48:29 +0300

    Update CoinRow.java

--- CoinRow/src/CoinRow.java
+++ CoinRow/src/CoinRow.java
@@ -5,26 +5,18 @@ public class CoinRow {
 		
 		int[] myArr = {5, 1, 2, 10, 6, 2};
 		System.out.println("Largest number: " + maxRowCoin(myArr));
-
 	}
-	
 	public static int maxRowCoin(int[] arr) {
 		
 		int len = arr.length;
-		
-		// create empty array
 		int[] dp = new int[len];
 		
-		//dp[0] = 0;
-		//dp[1] = arr[1];
 		dp[0] = arr[0];
 		dp[1] = Math.max(arr[0], arr[1]);
-		
 		
 		for (int i = 2; i < len; i++) {
 			dp[i] = Math.max(arr[i] + dp[i-2], dp[i-1]);
 		}
-		
 		
 		return dp[len-1];
 	}
